92eae3dc985468e20aa5013cea6f7ddf3283f51d
BastianVIA <70633697+BastianVIA@users.noreply.github.com>
2023-11-20 18:57:58 +0100

    Import audio

--- Assets/Scripts/Car/DrivingInstructorAudioManager.cs
+++ Assets/Scripts/Car/DrivingInstructorAudioManager.cs
@@ -5,12 +5,27 @@ namespace Car
 {
     public class DrivingInstructorAudioManager : MonoBehaviour
     {
-        [FormerlySerializedAs("startAudio")]
         [SerializeField] private AudioClip startCityAudio;
-        [SerializeField] private AudioClip turnLeftAudio;
-        [SerializeField] private AudioClip turnRightAudio;
+        [SerializeField] private AudioClip turnLeftAudio1;
+        [SerializeField] private AudioClip turnLeftAudio2;
+        [SerializeField] private AudioClip turnLeftAudio3;
+        [SerializeField] private AudioClip turnLeftAudio4;
+        [SerializeField] private AudioClip turnRightAudio1;
+        [SerializeField] private AudioClip turnRightAudio2;
+        [SerializeField] private AudioClip turnRightAudio3;
+        [SerializeField] private AudioClip turnRightAudio4;
         [SerializeField] private AudioClip finishLessonAudio;
         [SerializeField] private AudioClip parkAudio;
+        [SerializeField] private AudioClip freeroamAudio;
+        [SerializeField] private AudioClip redLightAudio1;
+        [SerializeField] private AudioClip redLightAudio2;
+        [SerializeField] private AudioClip speedLimitAudio;
+        [SerializeField] private AudioClip indicatorAudio1;
+        [SerializeField] private AudioClip indicatorAudio2;
+        [SerializeField] private AudioClip slowDownAudio;
+        [SerializeField] private AudioClip rightOfWayAudio;
+        [SerializeField] private AudioClip fullStopAudio;
+        
 
         private AudioSource audioSource;
 
@@ -31,18 +46,19 @@ namespace Car
         }
         public void PlayStartFreeRoam()
         {
-            
+            audioSource.clip = freeroamAudio;
+            audioSource.Play();
         }
 
         public void PlayTurnLeft()
         {
-            audioSource.clip = turnLeftAudio;
+            audioSource.clip = turnLeftAudio1;
             audioSource.Play();
         }
     
         public void PlayTurnRight()
         {
-            audioSource.clip = turnRightAudio;
+            audioSource.clip = turnRightAudio1;
             audioSource.Play();
         }
         public void PlayFinishLesson()
@@ -53,6 +69,36 @@ namespace Car
         public void PlayPark()
         {
             audioSource.clip = parkAudio;
+            audioSource.Play();
+        }
+
+        public void PlayRedLightViolation()
+        {
+            audioSource.clip = redLightAudio1;
+            audioSource.Play();
+        }
+
+        public void PlaySpeedViolation()
+        {
+            audioSource.clip = speedLimitAudio;
+            audioSource.Play();
+        }
+
+        public void PlayIndicatorViolation()
+        {
+            audioSource.clip = indicatorAudio1;
+            audioSource.Play();
+        }
+
+        public void PlayRightOfWayViolation()
+        {
+            audioSource.clip = rightOfWayAudio;
+            audioSource.Play();
+        }
+
+        public void PlayFullStopViolation()
+        {
+            audioSource.clip = fullStopAudio;
             audioSource.Play();
         }
     }
